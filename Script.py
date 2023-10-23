37e320093ea02b45b989d80024714652f78bc33c
STILL ALIVE #OFFLINE <95173294+StillAliveTG@users.noreply.github.com>
2023-10-23 10:13:50 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -6,7 +6,7 @@ Mʏ Nᴀᴍᴇ Is ꧁ Rᴏᴄᴋʏ K.G.Ғ ꧂, I Cᴀɴ Pʀᴏᴠɪᴅᴇ Mᴏ�
 Hᴇʀᴇ Is Tʜᴇ Hᴇʟᴘ Fᴏʀ Mʏ Cᴏᴍᴍᴀɴᴅs.</b>"""
 
     ABOUT_TXT = """<b>✯ Mʏ Nᴀᴍᴇ: {}
-✯ Cʀᴇᴀᴛᴏʀ: <a href='https://t.me/creatorbeatz'>Sʜᴀʀᴠɪɴ Bᴀꜱɪʟ</a>
+✯ Cʀᴇᴀᴛᴏʀ: <a href='https://t.me/sharvin_basil'>Sʜᴀʀᴠɪɴ Bᴀꜱɪʟ</a>
 ✯ Lɪʙʀᴀʀʏ: <a href='https://docs.pyrogram.org/'>Pʏʀᴏɢʀᴀᴍ</a>
 ✯ Lᴀɴɢᴜᴀɢᴇ: <a href='https://www.python.org/download/releases/3.0/'>Pʏᴛʜᴏɴ 3</a>
 ✯ DᴀᴛᴀBᴀsᴇ: <a href='https://www.mongodb.com/'>MᴏɴɢᴏDB</a>
